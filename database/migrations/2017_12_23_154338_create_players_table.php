a6d950baedce92b99e7f2555994166bb3f2f5853
Omar Ayman <omar@sandbox.dev>
2017-12-23 17:56:22 +0200

    all migrations are created

---
+++ database/migrations/2017_12_23_154338_create_players_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePlayersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('players', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->integer('age');
+            $table->string('country');
+            $table->timestamps();
+            $table->integer('club_id')->unsigned();
+
+            $table->foreign('club_id')->references('id')->on('clubs');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('players');
+    }
+}
